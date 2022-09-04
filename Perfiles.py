5019c5e17a66744e3bf4f8e7425af0d2b92cb1cb
Alejandro Gutiérrez M <alejandromartinez10@live.com>
2022-09-03 21:50:43 -0500

    Add files via upload

---
+++ Perfiles.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Alejandro Gutiérrez
+"""
+
+import pandas as pd
+import seaborn as sb
+ 
+
+# =============================================================================
+# EDA: Exploratory Data Analysis
+# =============================================================================
+
+# Cargar archivo con perfiles de instagram formato csv separado por tabulador
+perfiles = pd.read_csv(r'C:\Users\Alejandro\Prueba\instagram_profiles.csv',sep='\t')
+print('perfiles cargado')
+
+# =============================================================================
+# 1. Información Básica sobre el DataSet'''
+# =============================================================================
+#Vista Previa
+print(perfiles.head())
+#Dimensionalidad
+print('El DataSet tiene {} filas y {} columnas'.format(perfiles.shape[0],perfiles.shape[1])) 
+#Describir la información
+print(perfiles.describe().apply(lambda x: x.apply('{0:.0f}'.format)))
+#Información basica - tipos de datos
+print(perfiles.info())
+# =============================================================================
+# 2. Valores Duplicados'''
+# =============================================================================
+#Valores duplicados en el dataframe
+print('El DataSet tiene {} registros duplicados'.format(perfiles.duplicated().sum()))
+
+# =============================================================================
+# 3. Valores Nulos
+# =============================================================================
+print(perfiles.isnull().sum())
+#Elimina filas que tengan en los campos n_posts y profile_id valores nulos
+perfiles = perfiles.dropna(axis=0, subset=['n_posts'])
+perfiles = perfiles.dropna(axis=0, subset=['profile_id'])
+#Reemplaza valores nulos por texto definido
+perfiles["description"].fillna("No Description", inplace = True)
+perfiles["is_business_account"].fillna("Not Defined", inplace = True)
+perfiles["firstname_lastname"].fillna("No Name", inplace = True)
+print(perfiles.isnull().sum())
+# =============================================================================
+# 4. Valores Únicos
+# =============================================================================
+print(perfiles['is_business_account'].unique())
+print(sb.countplot(perfiles['is_business_account']))
+
+# =============================================================================
+# 5. Eliminar columnas innecesarias en el modelo
+# =============================================================================
+perfiles = perfiles.drop(['url'], axis=1)
+perfiles = perfiles.drop(['cts'], axis=1)
+
+#Casteo tipo de datos
+#perfiles['profile_id'] = pd.to_numeric(perfiles['profile_id'], errors='ignore')
+#perfiles['profile_id'] = perfiles['profile_id'].apply(lambda x: x.apply('{0:.0f}'.format))
+#Elimina todas las filas que tengan valores nulos
+#posts = pd.read_parquet(r'C:\Users\Alejandro\Prueba\instagram_posts.parquet', engine='pyarrow')
+#print('posts cargado')
+
+perfiles.to_csv(r'C:\Users\Alejandro\Prueba\resultado_prueba.csv',sep=',')
