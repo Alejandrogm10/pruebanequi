ec89ec623e6aebceabda5a4df305d2e962e59210
Alejandro Gutiérrez M <alejandromartinez10@live.com>
2022-09-04 19:55:17 -0500

    Update ensayo.py

--- src/ensayo.py
+++ src/ensayo.py
@@ -6,4 +6,17 @@ Created on Sun Sep  4 18:42:25 2022
 """
 
 
-import pandas
+import pandas as pd
+import boto3
+import io
+
+# =============================================================================
+# EDA: Exploratory Data Analysis
+# =============================================================================
+
+# Cargar archivo con perfiles de instagram formato csv separado por tabulador
+s3_file_key = 'instagram_profiles2.csv'
+bucket = 'insumosprueba'
+s3 = boto3.client('s3')
+obj = s3.get_object(Bucket=bucket, Key=s3_file_key)
+perfiles = pd.read_csv(io.BytesIO(obj['Body'].read()),sep=',')
